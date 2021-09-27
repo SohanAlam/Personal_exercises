cc646df03ede84ca8f699a53014ecb90dd3c5831
Sohan ul alam <sohanulalam1996@gmail.com>
2021-09-27 19:05:26 +0200

    done for today(27th september)

--- python.py
+++ python.py
@@ -1,3 +1,15 @@
-# x = 2+2
-# print(x)
-# print("hellö")
+import pandas as pd
+import numpy as np
+from pandas.core.frame import DataFrame
+
+
+data = {'animal': ['cat', 'cat', 'snake', 'dog', 'dog', 'cat', 'snake', 'cat', 'dog', 'dog'],
+        'age': [2.5, 3, 0.5, np.nan, 5, 2, 4.5, np.nan, 7, 3],
+        'visits': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'priority': ['yes', 'yes', 'no', 'yes', 'no', 'no', 'no', 'yes', 'no', 'no']}
+
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(data , index=labels)
+
+df[['animal','age']]
