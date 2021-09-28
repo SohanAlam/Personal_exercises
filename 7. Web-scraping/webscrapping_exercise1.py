ea6d4259c57de60a43109a01f77a75d934ba86c4
Sohan ul alam <sohanulalam1996@gmail.com>
2021-09-28 14:54:22 +0200

    Update webscrapping_exercise1.py

--- 7. Web-scraping/webscrapping_exercise1.py
+++ 7. Web-scraping/webscrapping_exercise1.py
@@ -8,15 +8,23 @@ soup = BeautifulSoup(page.content, 'html.parser')
 #print(page)
 
 
-day_name = soup.find_all('p', class_='period-name')
-#print(day_name)
+day_name = soup.find('div', class_='col-sm-2 forecast-label')
+#print(day_name.text)
 date = soup.find_all('p', class_='text-right') #problem when i am trying to add date
 #print(date)
-short_description = soup.find_all('p', class_='panel-body')
-#print(short_description)
-temp1 = soup.find_all('p', class_='myforecast-current-lrg')
-temp2 = (temp1 - 32) * 5/9
-celsius = round(temp2,2)
-print(celsius)
+short_description = soup.find('div',id='detailed-forecast')
+#print(short_description.text)
+#temp1 = soup.find('p', class_='myforecast-current-lrg')
+#print(temp1.get_text())
+
+#temp2 = ((int(temp1) - 32) * 5/9)
+#celsius = round(temp2,2)
+#print(celsius)
+results = soup.find(class_='contentArea')
+dates = results.find('div', class_='panel-body', id='seven-day-forecast-body')
+#print(dates.text)
+day_forecast = soup.find('div', class_="panel-body",id = "seven-day-forecast-body")
+print(day_forecast.text)
+
 
 
