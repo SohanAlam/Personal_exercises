f7a6e2b77db7442225f7faff559245df2fa1ca92
Sohan ul alam <sohanulalam1996@gmail.com>
2021-10-02 22:13:38 +0200

    Update quiz.py

--- 9. Linear Algebra/quiz.py
+++ 9. Linear Algebra/quiz.py
@@ -5,9 +5,16 @@ import seaborn as sbn
 
 
 c_list = [1,2]
-print("The list:",c_list)
-print("Has length:", len(c_list))
+#print("The list:",c_list)
+#print("Has length:", len(c_list))
 
 c_vector = np.array(c_list)
-print("The vector:", c_vector)
-print("Has shape:",c_vector.shape)
+#print("The vector:", c_vector)
+#print("Has shape:",c_vector.shape)
+
+A = np.arange(6).reshape((3,2))
+C = np.random.randn(2,2)
+
+print( A.shape)
+print( C.shape)
+
