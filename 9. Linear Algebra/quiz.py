d1c757aec6ca7e4bcf382fbfcfb149e45d44209c
Sohan ul alam <sohanulalam1996@gmail.com>
2021-10-02 22:11:53 +0200

    Update quiz.py

---
+++ 9. Linear Algebra/quiz.py
@@ -0,0 +1,13 @@
+import sympy as sympy
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sbn
+
+
+c_list = [1,2]
+print("The list:",c_list)
+print("Has length:", len(c_list))
+
+c_vector = np.array(c_list)
+print("The vector:", c_vector)
+print("Has shape:",c_vector.shape)
