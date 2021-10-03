02f08b65564ae96b1d797e7b70da9bded36dd779
Sohan ul alam <sohanulalam1996@gmail.com>
2021-10-03 22:22:37 +0200

    Update pythoin_function_basics.py

---
+++ pythoin_function_basics.py
@@ -0,0 +1,11 @@
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
