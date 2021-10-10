81e6d4073bcd5b716ef6923e107fac07529a1f58
Sohan ul alam <sohanulalam1996@gmail.com>
2021-10-10 22:22:47 +0200

    Update exercise.py

--- 13. Statistics/exercise.py
+++ 13. Statistics/exercise.py
@@ -2,3 +2,9 @@ import numpy as np
 mylist = list('abcedfghijklmnopqrstuvwxyz')
 myarr = np.arange(26)
 mydict = dict(zip(mylist, myarr)
+
+
+mylist = list('abcedfghijklmnopqrstuvwxyz')
+myarr = np.arange(26)
+mydict = dict(zip(mylist, myarr))
+ser = pd.Series(mydict)
