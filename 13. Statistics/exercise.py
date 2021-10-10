f4b66eefaf00384ecc9beee9b4c09120f46b9fab
Sohan ul alam <sohanulalam1996@gmail.com>
2021-10-10 22:21:58 +0200

    Update exercise.py

--- 13. Statistics/exercise.py
+++ 13. Statistics/exercise.py
@@ -1,26 +1,4 @@
-'''from matplotlib import pyplot
-get_ipython().run_line_magic('matplotlib', 'inline')
-
-#Import rcParams to set font styles
-from matplotlib import rcParams
-
-#Set font style and size 
-rcParams['font.family'] = 'serif'
-rcParams['font.size'] = 16'''
-
-
-ibu_null =pandas.isnull(ibu_s)
-abv_null =pandas.isnull(abv_s)
-
-
-# In[32]:
-
-
-ibu_null
-
-
-# In[69]:
-
-
-ibu_new=ibu_s.dropna()
-abv_new=abv_s.dropna()
+import numpy as np
+mylist = list('abcedfghijklmnopqrstuvwxyz')
+myarr = np.arange(26)
+mydict = dict(zip(mylist, myarr)
