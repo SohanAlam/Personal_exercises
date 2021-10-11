cbf303b0863b090b7d7bcc32883b7f29e09448b7
Sohan ul alam <sohanulalam1996@gmail.com>
2021-10-11 15:33:17 +0200

    selenium_1st page

--- Build Week 1/Day1_Webscraping and analysis/webscrapping_using_selenium.py
+++ Build Week 1/Day1_Webscraping and analysis/webscrapping_using_selenium.py
@@ -1,27 +1,50 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from time import sleep
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
+from time import sleep
 
-page = "https://www.goodreads.com/list/show/50.The_Best_Epic_Fantasy_fiction_"
-my_driver = ("C:\practice_september\Personalexercises_september\Build Week 1\Day1_Webscraping and analysis\chromedriver.exe")
+url = 'https://www.goodreads.com/list/show/50.The_Best_Epic_Fantasy_fiction_'
+driver = webdriver.Chrome()
+e_info = 'soghuniy@gmail.com'
+pass_info = 'soghuniy_96'
 
+############### scraping info about books #####################################
+titles = []
+urls = []
+authors = []
+avg_ratings = []
+num_ratings = []
+driver.get(url)
 
-driver = webdriver.Chrome(my_driver)
-driver.create_options()
-driver.minimize_window()
-driver.get(page)
-sleep(3)
+for i in range(1,3):
+    if i ==2:
+        sleep(3)
+        x_button = driver.find_element_by_xpath('/html/body/div[3]/div/div/div[1]/button/img')
+        x_button.click()
+    book_table = driver.find_element_by_xpath('//table[@class="tableList js-dataTooltip"]')
+    books = book_table.find_elements_by_tag_name('tr')
 
-url_
+    for book in books:
+        info = book.find_elements_by_tag_name('td')[2]
+        b_title = info.find_element_by_class_name('bookTitle')
+        titles.append(b_title.text)
+        urls.append(b_title.get_attribute('href'))
+        author = info.find_elements_by_tag_name('span')[2].find_element_by_tag_name('span')
+        authors.append(author.text)
+        rating_text = info.find_elements_by_class_name('minirating')[0].text
+        # this text consists of -> avg rating + 'avg rating' +'-' + num rating + 'ratings', we should split and choose only info we need
+        avg_ratings.append(rating_text.split()[0])
+        num_ratings.append(rating_text.split()[4].replace(',',''))
+    next_button = driver.find_element_by_xpath('//a[@class="next_page"]')
+    try:
+        next_button.click()
+    except:
+        print(print('it was last one'))
 
-table= driver.find_elements_by_xpath('//*[@id="all_votes"]/table')
-all_elements = table.find
-print(title)
+    sleep(2)
 
-
+print(titles)
+print(urls)
+print(authors)
+print(avg_ratings)
+print(num_ratings)
+# driver.quit()
---
+++ Build Week 1/Day1_Webscraping and analysis/webscrapping_using_soup.py
@@ -0,0 +1,25 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+
+page = requests.get("https://www.goodreads.com/list/show/50.The_Best_Epic_Fantasy_fiction_")
+soup = BeautifulSoup(page.content, 'html.parser')
+
+url=[]
+title=[]
+author=[]
+reviews=[]
+rating=[]
+
+for i in range(1,3):
+    page1 = requests.get("https://www.goodreads.com/list/show/50.The_Best_Epic_Fantasy_fiction_?page={i}")
+    soup1 = BeautifulSoup(page1.content, 'html.parser')
+    all1=soup1.find_all('table',class_='tableList js-dataTooltip')
+
+    for item in all1():
+        for link in item.find_all('a',class_='bookTitle',href=true):
+            url.append(baseurl+link[href])
+
+print(len(url))
+
