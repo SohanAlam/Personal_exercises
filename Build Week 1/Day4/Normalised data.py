3bb55e2edb597536789c1b5631c6d1da5c62b28e
Sohan ul alam <sohanulalam1996@gmail.com>
2021-10-14 13:29:45 +0200

    Update Normalised data.py

--- Build Week 1/Day4/Normalised data.py
+++ Build Week 1/Day4/Normalised data.py
@@ -122,6 +122,3 @@ df = df3.to_csv('final_7.csv',sep=',', na_rep='', float_format=None, columns=Non
 df.dtypes
 
 
-
-#path_or_buf=None, sep=',', na_rep='', float_format=None, columns=None, header=True, index=True, index_label=None, mode='w', encoding=None, compression='infer', quoting=None, quotechar='"', line_terminator=None, chunksize=None, date_format=None, doublequote=True, escapechar=None, decimal='.', errors='strict', storage_options=Non
-
