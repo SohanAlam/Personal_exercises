f3b15c6ca8cb4c44db3f5085188ad7327909b27c
Sohan ul alam <sohanulalam1996@gmail.com>
2021-11-12 10:54:43 +0100

    Flask data base

--- Build Week 2(Machine Learning)/Day2/app.py
+++ Build Week 2(Machine Learning)/Day2/app.py
@@ -1,14 +1,20 @@
 from flask import Flask , render_template
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+#creating database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sohan.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
 
 @app.route("/")
 def hello_world():
     return render_template('index.html')
-    #return "<p>Hello World!</p>"
+     #return "<p>Hello World!</p>"
 @app.route("/products")
 def products():
     return "<p>This is products!</p>"
-
 if __name__ == "__main__":
     app.run(debug=True)
