58062cdd32bf446957569e96a57708a13a4f33d8
Sohan ul alam <sohanulalam1996@gmail.com>
2022-01-21 16:03:56 +0100

    python

---
+++ Chapter1/python for me/task1.py
@@ -0,0 +1,20 @@
+array1 = {30}
+
+def task1(array1):
+    
+    for num in array1:
+        if num % 3 == 0:
+                print( "Strive")
+        else:
+            pass
+        if num % 5 == 0:
+                print( "School ")
+        else:
+            pass
+
+        if num % 3 == 0  and  num % 5==0:
+                    print( "strive School ")
+        else:
+            pass
+
+task1(array1)
