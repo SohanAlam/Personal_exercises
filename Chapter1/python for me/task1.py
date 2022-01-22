115f7af7d0ac471c5ae75b84e94c8872361bcdf1
Sohan ul alam <sohanulalam1996@gmail.com>
2022-01-22 01:11:57 +0100

    Update task1.py

--- Chapter1/python for me/task1.py
+++ Chapter1/python for me/task1.py
@@ -2,16 +2,16 @@
 
 from numpy import array
 
-array1 = [1,1,1,2,2,2,3,3,3]
+array1 = [1,1,3,5,15]
 def task1(array1):
     
     for num in array1:
-        if num % 3 == 0:
-                print( "Strive")
+        if (num % 3 == 0)  and  (num % 5==0):
+            print( "strive School ")
+        elif num % 3 == 0:
+            print( "Strive")
         elif num % 5 == 0:
             print( "School ")
-        elif num % 3 == 0  and  num % 5==0:
-            print( "strive School ")
         else:
             print(num)
 task1(array1)
