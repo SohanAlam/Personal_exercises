7b4b33472066fd88ecd20f41b7497530d7007d76
Sohan ul alam <sohanulalam1996@gmail.com>
2022-02-20 23:13:15 +0100

    HTML

--- Chapter1/python for me/task1.py
+++ Chapter1/python for me/task1.py
@@ -4,7 +4,6 @@ from numpy import array
 
 array1 = [1,1,3,5,15]
 def task1(array1):
-    
     for num in array1:
         if (num % 3 == 0)  and  (num % 5==0):
             print( "strive School ")
